a7d45ecb0ca69290e22c3615408fb37b135ef07c
Wayne M <waynemot@msn.com>
2021-10-24 21:07:41 -0500

    chg config routes w/calendar path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'calendar/index'
+  get 'calendar/new'
   get 'home/index'
   root 'home#index'
 
   # Add future routes here
   # Add route for OmniAuth callback
   match '/auth/:provider/callback', :to => 'auth#callback', :via => [:get, :post]
-  
+  get 'auth/signout'
+  get 'calendar', :to => 'calendar#index'
+
 end
